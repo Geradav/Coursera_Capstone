1af92d01d9b10973c3b9956db37789d16a007618
David Gerard <48278193+Geradav@users.noreply.github.com>
2019-09-08 13:17:09 +0300

    Jupyter notebook for Capstone Project
    Jupyter notebook used for the Capstone Project in IBM Data Science Professional Certificate

---
+++ Coursera-DG-CapstoneProject.py
@@ -0,0 +1,13 @@
+#%% [markdown]
+# ## Capstone project - *(IBM Data Science)* 
+# Student author: **David Gerard** 
+# <br><br>
+# This Jupiter notebook is used for the capstone project of the IBM Data Science Professional Certificate
+
+#%%
+import pandas as pd 
+import numpy as np
+
+
+#%%
+print("Hello Capstone Project Course!")
